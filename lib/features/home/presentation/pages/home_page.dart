60a1e2c561abf48047774010e19ada14a1d317c7
Abdul Aziz <abdulaziz5611@gmail.com>
2025-01-29 18:36:13 +0500

    bottom navigation bar added

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -15,6 +15,25 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+          bottomNavigationBar: BottomNavigationBar(
+            unselectedIconTheme: IconThemeData(color: AppColors.white),
+              selectedIconTheme: IconThemeData(color: AppColors.white),
+              selectedItemColor: AppColors.white,
+              unselectedItemColor: AppColors.white,
+              backgroundColor: Colors.black,
+              type: BottomNavigationBarType.fixed,
+              items: [
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.home_filled), label: 'Home'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.search_rounded), label: 'Search'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.menu_book_sharp), label: 'Your Library'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.add), label: 'Create'),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.workspace_premium), label: 'Premium'),
+              ]),
           drawer: Drawer(
             child: Container(
                 padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
@@ -49,42 +68,107 @@ class _HomePageState extends State<HomePage> {
           ),
           appBar: AppBar(
             backgroundColor: AppColors.black,
-            leading: Builder(builder: (context) {
-              return GestureDetector(
-                onTap: () {
-                  Scaffold.of(context).openDrawer();
-                },
-                child: Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: AppColors.blue,
-                  ),
-                  child: Center(
-                      child: Text(
-                    'A',
-                    style: TextStyle(fontSize: 20),
-                  )),
-                ),
-              );
-            }),
           ),
-          body: Container(padding: EdgeInsets.symmetric(horizontal: 10),
-            color: AppColors.black,
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Your top mixes',
-                  style: TextStyle(color: AppColors.white, fontSize: 24),
-                ),
-                SizedBox(height: 10,),
-                Container(
+          body: SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              color: AppColors.black,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 7.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              color: AppColors.grey,
+                              borderRadius: BorderRadius.circular(5)),
+                          height: 50,
+                          width: 160,
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 50,
+                                width: 50,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(5),
+                                  child: Image.asset(
+                                    AppImages.profileImage,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Text('add singer/song\n title here')
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          width: 2,
+                        ),
+                        Container(
+                          height: 50,
+                          width: 160,
+                          decoration: BoxDecoration(
+                              color: AppColors.grey,
+                              borderRadius: BorderRadius.circular(5)),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 50,
+                                width: 50,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(5),
+                                  child: Image.asset(
+                                    AppImages.profileImage,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Text('add singer/song\n title here')
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    'Your top mixes',
+                    style: TextStyle(color: AppColors.white, fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SongsListView(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Recommended for today',
+                    style: TextStyle(color: AppColors.white, fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SongsListView(),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Popular radio',
+                    style: TextStyle(color: AppColors.white, fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  SongsListView(),
 
-                  width: double.infinity,
-                  height: 150,
-                  child: SongsListView()
-                ),
 
-              ],
+                ],
+              ),
             ),
           )),
     );
@@ -105,5 +189,3 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
-
-
--- lib/features/home/presentation/pages/widgets/songs_list_view.dart
+++ lib/features/home/presentation/pages/widgets/songs_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:spotify_clone/core/app_configs/assets/app_images.dart';
+import 'package:spotify_clone/core/app_configs/theme/app_colors.dart';
 
 class SongsListView extends StatelessWidget {
   const SongsListView({
@@ -8,33 +9,38 @@ class SongsListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 10,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index) {
-          return Container(
-            margin: EdgeInsets.symmetric(horizontal: 5),
-            color: Colors.red,
-            height: 150,
-            width: 150,
-            child: Stack(
-              children: [
-                Positioned.fill(
-                    child: Image.asset(
-                      AppImages.profileImage,
-                      fit: BoxFit.cover,
-                    )),
-                Positioned(
-                    bottom: 2,
-                    left: 0,
-                    right: 0,
-                    child: Text(
-                      'Add some descriptions here',
-                      textAlign: TextAlign.center,
-                    ))
-              ],
-            ),
-          );
-        });
+    return Container(height: 150,
+      width: double.infinity,
+      child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.symmetric(horizontal: 5),
+              height: 150,
+              width: 150,
+              decoration: BoxDecoration(borderRadius: BorderRadius.circular(5),),
+              child: Stack(
+                children: [
+                  Positioned.fill(
+                      child: ClipRRect(borderRadius: BorderRadius.circular(5),
+                        child: Image.asset(
+                          AppImages.profileImage,
+                          fit: BoxFit.cover,
+                        ),
+                      )),
+                  Positioned(
+                      bottom: 2,
+                      left: 0,
+                      right: 0,
+                      child: Text(
+                        'Add some descriptions here',
+                        textAlign: TextAlign.center,
+                      ))
+                ],
+              ),
+            );
+          }),
+    );
   }
 }
